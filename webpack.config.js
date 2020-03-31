edf7560b249eb7bb021cd18fab1e6949ff921b1e
shren <shren@streamax.com>
2020-03-31 17:26:58 +0800

    autoprefixer

--- webpack.config.js
+++ webpack.config.js
@@ -16,9 +16,14 @@ module.exports = {
                     }
                 }
             },
-            { 
+            {
                 test: /\.scss$/,
-                use: ["style-loader", "css-loader", "sass-loader"]
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader",
+                    "postcss-loader"
+                ]
             }
         ]
     },
