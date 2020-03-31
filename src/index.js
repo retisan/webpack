511fa2c504d615e65077c3c02ad2a98e67266441
shren <shren@streamax.com>
2020-03-31 14:07:57 +0800

    update gitignore

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import jpg from "./test2.jpg";
-console.warn(jpg);
+import "./index.css";
 
 const img = new Image();
 img.src = jpg;
+img.classList.add("img");
 document.body.appendChild(img);
--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,10 @@ module.exports = {
                         limit: 10240
                     }
                 }
+            },
+            { 
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
             }
         ]
     },
