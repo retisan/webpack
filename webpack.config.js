400ecfef91378c9b285b0be7fbbba1c087800426
shren <shren@streamax.com>
2020-04-07 17:44:34 +0800

    使用babel/polyfill处理es6的函数对象

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,14 @@ module.exports = {
                 use: {
                     loader: "babel-loader",
                     options: {
-                        presets: ["@babel/preset-env"],
+                        presets: [
+                            [
+                                "@babel/preset-env",
+                                {
+                                    useBuiltIns: "entry",
+                                },
+                            ],
+                        ],
                     },
                 },
             },
