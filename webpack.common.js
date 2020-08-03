6f6cab214bd838e199bc51c04a971cb18466da8d
shren <shren@streamax.com>
2020-08-03 15:10:03 +0800

    使用HardSourceWebpackPlugin提升打包速度

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,7 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
 const path = require("path");
 
 module.exports = {
@@ -65,5 +66,6 @@ module.exports = {
             filename: "[name].css",
             chunkFilename: "[id].css",
         }),
+        new HardSourceWebpackPlugin()
     ],
 };
